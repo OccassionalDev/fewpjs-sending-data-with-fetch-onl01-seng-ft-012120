d91d7beeacce99c7c01265830c6db34e8b76daee
Erica Schimmel <ericaschimmel01@gmail.com>
2020-05-05 16:30:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 // Add your code here
 
-let configurationObject = {
+configurationObject = {
   method: "POST",
   headers: {
     "Content-Type": "application/json",
     "Accept": "application/json"
-  }
+  },
+  body: JSON.stringify({
+    dogName: "Byron",
+    dogBreed: "Poodle"
+  })
 };
 
 fetch(destinationURL, configurationObject);
