6203b0e7ace531d9f41dd10293597a2fc63de14d
Erica Schimmel <ericaschimmel01@gmail.com>
2020-05-05 16:39:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ function submitData(name, email) {
   };
   
   let configObj = {
-    
-  };
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(formData)
+};
   
   return fetch("http://localhost:3000/users", configObj);
 }
